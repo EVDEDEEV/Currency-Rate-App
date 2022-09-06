aa92ccdfe4f60d91fa08c2b92ae31e35c57d5ed2
Erkesh <dedeeverkesh@gmail.com>
2022-09-06 18:27:05 +0300

    Added Android Navigation Fragment on MainActivity.kt

--- app/src/main/java/my/project/currenciestestapp/presentation/MainActivity.kt
+++ app/src/main/java/my/project/currenciestestapp/presentation/MainActivity.kt
@@ -1,12 +1,36 @@
 package my.project.currenciestestapp.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import my.project.currenciestestapp.R
+import my.project.currenciestestapp.data.api.ApiConstants.BASE_URL
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    val TAG = "aboba"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostFragment)
+
+        setupActionBarWithNavController(navController)
+
+
+
+        Log.d(TAG, BASE_URL)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+
 }
