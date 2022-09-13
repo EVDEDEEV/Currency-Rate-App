c0998b9ef2f882ab7a8cff3240b4cf860f0cae3f
Erkesh <dedeeverkesh@gmail.com>
2022-09-13 18:08:02 +0300

    App is working with changed items in spinner(fixed crash when switching between fragments )

--- app/src/main/java/my/project/currenciestestapp/presentation/fragments/currencyFragment/CurrencyListFragment.kt
+++ app/src/main/java/my/project/currenciestestapp/presentation/fragments/currencyFragment/CurrencyListFragment.kt
@@ -5,19 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
-import android.widget.Toast
-import androidx.core.view.isInvisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
-import my.project.currenciestestapp.R
 import my.project.currenciestestapp.databinding.FragmentCurrencyListBinding
-import my.project.currenciestestapp.presentation.models.RatesUiModel
-import my.project.currenciestestapp.utils.Constants.API_KEY
 
 @AndroidEntryPoint
 class CurrencyListFragment : Fragment() {
@@ -31,15 +25,11 @@ class CurrencyListFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View {
         binding = FragmentCurrencyListBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
         setDataFromApiToLocalUiModel()
         initRecyclerView()
         setDataToRecyclerView()
         selectedCurrencyItemListener()
+        return binding.root
     }
 
     private fun setDataFromApiToLocalUiModel() {
@@ -52,20 +42,21 @@ class CurrencyListFragment : Fragment() {
             object : AdapterView.OnItemSelectedListener {
                 override fun onItemSelected(
                     parent: AdapterView<*>,
-                    view: View,
+                    view: View?,
                     pos: Int,
                     id: Long,
                 ) {
-                    val baseCurrency = binding.currencyListSpinner.selectedItem.toString()
-                    viewModel.getRates(baseCurrency)
-                    viewModel.currencies.observe(viewLifecycleOwner) {
-                        currencyAdapter.submitList(it)
+                    if (pos >= 0) {
+                        val baseCurrency = binding.currencyListSpinner.selectedItem.toString()
+                        viewModel.getRates(baseCurrency)
                     }
                 }
 
-                override fun onNothingSelected(parent: AdapterView<*>?) {
-                }
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
             }
+        viewModel.currencies.observe(viewLifecycleOwner) {
+            currencyAdapter.submitList(it)
+        }
     }
 
     private fun setDataToRecyclerView() {
